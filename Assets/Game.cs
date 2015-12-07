725f84cec66fe1205a3a8e485498f9aeab4aa9d5
albertnez <albertmg7@gmail.com>
2015-12-07 05:50:02 +0100

    Added new wall colors

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,21 +5,26 @@ public class Game : MonoBehaviour {
 
 	static public float obstacleSpeed = 60.0f;
 	static public float gridSpeed; 
-	static public Material gridMaterial;
+	static public Material[] gridMaterials;
 	static public float wallDepthScale = 20.0f;
 
     static private int currentScore;
 
 	// Use this for initialization
 	void Start () {
-		gridMaterial = Resources.Load("Grid", typeof(Material))as Material;
-		float tiling = gridMaterial.mainTextureScale.y;
+        gridMaterials = new Material[] {
+            Resources.Load("Grid", typeof(Material))as Material,
+            Resources.Load("GridTop1", typeof(Material))as Material,
+        };
+		float tiling = gridMaterials[0].mainTextureScale.y;
 		gridSpeed = obstacleSpeed / (10.0f*wallDepthScale) * tiling;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gridMaterial.mainTextureOffset += new Vector2(0.0f, - gridSpeed * Time.deltaTime);
+        for (int i = 0; i < gridMaterials.Length; ++i) {
+            gridMaterials[i].mainTextureOffset += new Vector2(0.0f, - gridSpeed * Time.deltaTime);
+        }
 	}
 
     // Called each time we start from first level.
