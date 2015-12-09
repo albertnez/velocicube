c801410eeef53fe727e736a50fe37e0fb829d236
albertnez <albertmg7@gmail.com>
2015-12-09 04:25:53 +0100

    Menu with selectable buttons. Game can now start and exit

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -6,7 +6,9 @@ public class Game : MonoBehaviour {
 	static public float obstacleSpeed = 60.0f;
 	static public float wallDepthScale = 20.0f;
     static public float tiling = 20.0f;
-	static public float gridSpeed = obstacleSpeed / (10.0f * wallDepthScale) * tiling;
+    static public float gridSpeed = 0;
+	static private float gridGameSpeed = obstacleSpeed / (10.0f * wallDepthScale) * tiling;
+    static private float gridMenuSpeed = 0.5f;
     static private int currentScore = 0;
     static private int bestScore = 0;
 
@@ -17,6 +19,28 @@ public class Game : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	}
+
+    // Menu Functions
+    static public void Play() {
+        SetGame();
+    }
+
+    static public void Credits() {
+    }
+
+    static public void Exit() {
+        Application.Quit();
+    }
+
+    // Sets the values for the menu.
+    static public void SetMenu() {
+        gridSpeed = gridMenuSpeed;
+    }
+
+    // Sets the values for the game.
+    static public void SetGame() {
+        gridSpeed = gridGameSpeed;
+    }
 
     // Called each time we start from first level.
     static public void Restart() {
---
+++ Assets/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public UnityEngine.UI.Button playButton;
+
+	// Use this for initialization
+	void Start () {
+        // Set the speed of the tunnel.
+        Game.SetMenu();
+        // By default, select the Start button.
+        playButton.Select();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Menu Functions
+    public void Play() {
+        Game.SetGame();
+        Application.LoadLevel("MainScene");
+    }
+
+    public void Credits() {
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
