dd49016673b2c77f6086b56d80dd5c1b6d0b2205
alinimi <alicia.nicasmiquel@gmail.com>
2015-12-11 15:10:56 +0100

    gridSpeed = gridGameSpeed

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -6,14 +6,14 @@ public class Game : MonoBehaviour {
 	static public float obstacleSpeed = 60.0f;
 	static public float wallDepthScale = 20.0f;
     static public float tiling = 20.0f;
-    static public float gridSpeed = 0;
 	static private float gridGameSpeed = obstacleSpeed / (10.0f * wallDepthScale) * tiling;
     static private float gridMenuSpeed = 0.5f;
     static private int currentScore = 0;
     static private int bestScore = 0;
+    static public float gridSpeed = gridGameSpeed;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
